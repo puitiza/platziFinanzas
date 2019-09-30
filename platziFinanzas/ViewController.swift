3027de12237f2855a0862ca81c374660257047e7
Anthony Puitiza Mac <anthony.puitiza.02@gmail.com>
2019-09-30 15:47:19 -0500

    Initial Commit

---
+++ platziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  platziFinanzas
+//
+//  Created by Anthony Puitiza on 9/30/19.
+//  Copyright © 2019 Anthony Puitiza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
